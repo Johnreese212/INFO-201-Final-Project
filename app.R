ee613bf462b740148e4aa868c633c7e7efd78fe6
Michelle Ponting <michellep572@gmail.com>
2019-03-11 15:56:26 -0700

    add analysis and app for Q 3

--- app.R
+++ app.R
@@ -6,6 +6,8 @@ library("tidyr")
 library("maps")
 library("DT")
 
+source("analysis_a6.R")
+
 page_one <- tabPanel(
   "Overview"
 )
@@ -15,7 +17,26 @@ page_two <- tabPanel(
 )
 
 page_three <- tabPanel(
-  "Twitter Activity Analysis"
+  "Twitter Activity Analysis",
+  titlePanel("Analysis of President Trump's Twitter Activity in 2017"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput(
+        inputId = "month",
+        label = "Month",
+        min = 1,
+        max = 12,
+        value = 1,
+        ticks = FALSE
+      ),
+      h2(textOutput("monthname"))
+    ),
+    mainPanel(
+      plotOutput("plotmontlytweets"),
+      h1(textOutput("twitterdata")),
+      tableOutput("tablemonthlytweets")
+    )
+  )
 )
 
 page_four <- tabPanel(
@@ -28,24 +49,47 @@ page_five <- tabPanel(
 
 my_ui <- navbarPage(
   "My app",
-  page_one,
-  page_two,
   page_three,
+  page_two,
+  page_one,
   page_four,
   page_five
 )
 
-
 my_server <- function(input,output) {
   
+  # Michelle Q data
+  output$plotmontlytweets <- renderPlot({
+    data <- twitter_data
+
+    plot.data <- data %>% filter(month == input$month)
+    
+    ggplot(data = plot.data) +
+      geom_point(mapping = aes(x = day, y = time, color = is_retweet)) + 
+      scale_y_discrete(breaks = NULL) +
+      labs(
+        title = paste("All of Donald Trump's Tweets in", month.name[input$month]),
+        y = "Time of Day (00:00 to 24:00)",
+        x = "Day of the Month",
+        color = "Is is a Retweet?"
+      )
+  })
+  output$twitterdata <- renderText({
+    twitter_info <- get_monthly_info(input$month)
+    twitter_info
+  })
+  output$monthname <- renderText({
+    month <- month.name[input$month]
+    month
+  })
+  output$tablemonthlytweets <- renderTable({
+    table <- monthly_tweets
+    table
+  })
 }
 
 
 shinyApp(ui = my_ui, server = my_server)
-
-
-
-
 
 
 
