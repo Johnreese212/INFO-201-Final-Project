897cdaf0598c507151c48a7369f1c445e7a52706
John Reese <reesej5@uw.edu>
2019-03-11 14:16:38 -0700

    Added correlation to tab four

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 The final project in Informatics 201-Technical Foundations. In this project, we created a shiny app that uses data from Donald Trump's tweets and his approval ratings.
 
 This project was created by John Reese, Michelle Ponting, Roshni Sinha and Andy Straavaldson.
+
+Our questions:
+1.
+2.
+3.
+4.
--- app.R
+++ app.R
@@ -23,9 +23,11 @@ page_three <- tabPanel(
 
 page_four <- tabPanel(
   "Approval Rating and Tweeting Frequency", # Label for the tab in the navbar
+  titlePanel("How did changes in Trump's approval/disapproval rating affect how often he tweeted?"),
   sidebarLayout(
     sidebarPanel(
-      radioButtons(inputId = "approve", label = "Metric",choices = c("approve","disapprove"))
+      radioButtons(inputId = "approve", label = "Metric",choices = c("approve","disapprove")),
+      textOutput(outputId = "corr")
     ),
     mainPanel(
       plotOutput(outputId = "frequency_plot"),
@@ -54,15 +56,31 @@ my_ui <- navbarPage(
 
 my_server <- function(input,output) {
   output$frequency_plot <- renderPlot({
-    ggplot(data = daily_approval_and_frequency)+
-      geom_point(mapping = aes_string(x = "Frequency", y = input$approve)) +
-      geom_smooth(mapping = aes_string(x = "Frequency", y = input$approve))
+    ggplot(data = daily_approval_and_frequency,mapping = aes_string(x = "Frequency", y = input$approve)) +
+      geom_point() +
+      geom_smooth(method = "lm")
   })
   
   output$date_plot <- renderPlot({
     apr <- toString(input$approve)
     ggplot(data = daily_approval_and_frequency)+
       geom_point(mapping = aes_string(x = "Date", y = apr, color = "Frequency")) 
+  })
+  
+  output$corr <- renderText({
+    tex <- ""
+    if (input$approve == "approve") {
+      x <- as.numeric(daily_approval_and_frequency$approve)
+      y <- as.numeric(daily_approval_and_frequency$Frequency)
+      freq_cor <- round(cor(x,y),2)
+      tex <- paste("The correlation between approval rating and frequency is", freq_cor)
+    } else {
+      x <- as.numeric(daily_approval_and_frequency$disapprove)
+      y <- as.numeric(daily_approval_and_frequency$Frequency)
+      freq_cor <- round(cor(x,y),2)
+      tex <- paste("The correlation between disapproval rating and frequency is", freq_cor)
+    }
+    tex
   })
 }
 
